8e3fecc38cba9de576a7ae869a1a007a3c59ddaa
unknown <tanelt@laptop626.webmedia.int>
2014-06-05 23:06:46 +0300

    v_user_points

---
+++ db/migrate/20140605130306_add_v_user_points.rb
@@ -0,0 +1,23 @@
+# encoding: UTF-8
+class AddVUserPoints < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      CREATE OR REPLACE VIEW V_USER_POINTS AS
+        SELECT usr.id
+              ,usr.name
+              ,tour.id AS tournament_id
+              ,COALESCE((SELECT usre.points
+                  FROM user_results usre
+                 WHERE usre.user_id = usr.id
+                   AND usre.tournament_id = tour.id), 0) AS points
+          FROM users usr
+          LEFT OUTER JOIN tournaments tour ON 1 = 1
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DROP VIEW v_group_elements
+    SQL
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140605120306) do
+ActiveRecord::Schema.define(:version => 20140605130306) do
 
   create_table "games", :force => true do |t|
     t.integer  "team1_id",      :precision => 38, :scale => 0
