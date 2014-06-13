800b83d532286c3d01dc01c27fb4d9b34f515fa2
unknown <tanelt@laptop626.webmedia.int>
2014-06-13 10:58:38 +0300

    save empty game results fix

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,15 +15,13 @@ class GamesController < ApplicationController
     tournament_id = params[:tournament_id]
     userGames = UserGame.where(:user_id => user_id, :tournament_id => tournament_id)
     result.map do |k,v|
-      if v != nil && !v.empty?
-        existingGames = userGames == nil ? nil : userGames.select{|userGame| k.to_i == userGame.game_id}
-        if existingGames == nil || existingGames.empty?
-          UserGame.create!(:user_id => user_id, :game_id => k, :result => v, :tournament_id => tournament_id)
-        else
-          existingGame = existingGames[0]
-          existingGame.update_attribute(:result, v)
-        end
-      end 
+      existingGames = userGames == nil ? nil : userGames.select{|userGame| k.to_i == userGame.game_id}
+      if (existingGames == nil || existingGames.empty?) && (v != nil && !v.empty?)
+        UserGame.create!(:user_id => user_id, :game_id => k, :result => v, :tournament_id => tournament_id)
+      elsif existingGames != nil && !existingGames.empty?
+        existingGame = existingGames[0]
+        existingGame.update_attribute(:result, v)
+      end
     end
     user = User.find_by_id user_id
     if user.name == 'tegelikud tulemused'
