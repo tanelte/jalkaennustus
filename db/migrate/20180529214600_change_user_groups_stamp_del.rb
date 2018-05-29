73e837e1c3b6ae3e08a41232758194618b47d578
Tanel Tensing <tanel.tensing@nortal.com>
2018-05-29 21:57:52 +0300

    user_groups stamp_del

--- db/migrate/20180529214600_change_user_groups_stamp_del.rb
+++ db/migrate/20180529214600_change_user_groups_stamp_del.rb
@@ -1,9 +1,9 @@
 class ChangeUserGroupsStampDel < ActiveRecord::Migration
   def up
-    add_column :user_groups, :stamp_del, :timestamp
+    add_column :user_groups, :deleted_at, :timestamp
   end
 
   def down
-    remove_column :user_groups, :stamp_del
+    remove_column :user_groups, :deleted_at
   end
 end
